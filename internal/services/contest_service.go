c18c6e0ceb80b3bacffa8b0a9eeae18ab60529a9
MinhWalker <minhnh421999@gmail.com>
2023-02-21 01:58:58 +0700

    add services for contest

--- internal/services/contest_service.go
+++ internal/services/contest_service.go
@@ -1,1 +1,27 @@
 package services
+
+import (
+	"fiber-base-go/internal/model"
+	"fiber-base-go/internal/repository"
+	"github.com/pkg/errors"
+)
+
+type ContestService interface {
+	CreateContest(contest *model.Contest) error
+}
+
+type contestService struct {
+	contestRepo repository.ContestRepository
+}
+
+func NewContestService(contestRepo repository.ContestRepository) ContestService {
+	return &contestService{contestRepo}
+}
+
+func (c contestService) CreateContest(contest *model.Contest) error {
+	if err := c.contestRepo.Create(contest); err != nil {
+		return errors.Wrap(err, "contestService.CreateStudent")
+	}
+
+	return nil
+}
