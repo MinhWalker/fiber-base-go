4a8b796a54fd63ed6cf95669628c9b8d34907af5
MinhWalker <minhnh421999@gmail.com>
2023-02-21 01:58:44 +0700

    add repo to contest and find student by grade and class

--- internal/repository/contest_repo.go
+++ internal/repository/contest_repo.go
@@ -1,1 +1,27 @@
 package repository
+
+import (
+	"fiber-base-go/internal/model"
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+)
+
+type ContestRepository interface {
+	Create(contest *model.Contest) error
+}
+
+type contestRepository struct {
+	db *gorm.DB
+}
+
+func NewContestRepository(db *gorm.DB) ContestRepository {
+	return &contestRepository{db}
+}
+
+func (c *contestRepository) Create(contest *model.Contest) error {
+	if err := c.db.Create(contest).Error; err != nil {
+		return errors.Wrap(err, "contestRepository.Create")
+	}
+
+	return nil
+}
--- internal/repository/student_repo.go
+++ internal/repository/student_repo.go
@@ -15,6 +15,8 @@ type StudentRepository interface {
 	Delete(s *model.Student) error
 	FindOne(uint) (*model.Student, error)
 	Update(*model.Student) error
+	FindStudentByClass([]string) ([]*model.Student, error)
+	FindStudentByGrade([]string) ([]*model.Student, error)
 }
 
 type studentRepository struct {
@@ -23,6 +25,25 @@ type studentRepository struct {
 
 func NewStudentRepository(db *gorm.DB) StudentRepository {
 	return &studentRepository{db}
+}
+
+func (f *studentRepository) FindStudentByClass(class []string) ([]*model.Student, error) {
+	var students []*model.Student
+	result := f.db.Where("class IN (?)", class).Find(&students)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return students, nil
+}
+
+func (f *studentRepository) FindStudentByGrade(grades []string) ([]*model.Student, error) {
+	var students []*model.Student
+	result := f.db.Where("SUBSTRING(class, 1, 2) IN (?)", grades).Find(&students)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return students, nil
 }
 
 func (f *studentRepository) GetAllStudent() ([]*model.Student, error) {
